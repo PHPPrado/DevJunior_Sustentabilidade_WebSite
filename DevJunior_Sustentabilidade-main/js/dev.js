f02b7797167bb2b706e17fdc5bc93cc16c2d2936
Pedro Henrique do Prado <104167056+PHPPrado@users.noreply.github.com>
2022-06-19 11:56:31 -0300

    Add files via upload

---
+++ DevJunior_Sustentabilidade-main/js/dev.js
@@ -0,0 +1,3 @@
+function link(){
+    document.getElementById('hcor').style.color('#000000')
+}
---
+++ DevJunior_Sustentabilidade-main/js/sobre.js
@@ -0,0 +1,3 @@
+function x1(){
+   alert('Muito Obrigado, a Dev Júnior agradece o seu feedback!!');
+}
